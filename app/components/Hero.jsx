47dfe6b3ea44cc11dfa57d0214f99a1fde3f4d9a
Mohammed Usama <usamabhakrani@gmail.com>
2024-05-07 11:59:42 +0500

    styles and icons

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,29 +1,33 @@
 import Image from "next/image";
-import pizza from "../../public/pizza.jpg";
-import pizza1 from "../../public/pizza1.jpg";
+import pizza2 from "../../public/pizza2.jpg";
+import RightIcon from "./RightIcon";
 
 const Hero = () => {
   return (
-    <section className="grid grid-cols-2">
-      <div>
-        <h1 className="text-4xl">Everything is better with Pizza</h1>
+    <section className="hero">
+      <div className="py-8">
+        <h1 className="text-4xl font-semibold">
+          Everything is better with a{" "}
+          <span className="text-red-800">Pizza</span>
+        </h1>
         <p className="my-4 text-gray-500">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
           explicabo non mollitia aliquam odio voluptate?
         </p>
-        <div className="mx-2">
-          <button className="bg-red-800 text-white px-6 py-2 mr-2 rounded">
-            Order Now
-          </button>
-          <button className="text-black border border-gray-500 px-6 py-2 rounded">
+        <div className="flex gap-2">
+          <button className="text-gray-500 border border-gray-600 font-semibold px-6 py-2 rounded">
             Learn More
+          </button>
+          <button className="bg-red-800 text-white px-4 py-2 mr-2 rounded flex gap-2 font-semibold">
+            Order Now
+            <RightIcon />
           </button>
         </div>
       </div>
       <div className="relative">
         <Image
           objectFit="contain"
-          src={pizza1}
+          src={pizza2}
           alt="a big pizza"
           layout="fill"
         />
